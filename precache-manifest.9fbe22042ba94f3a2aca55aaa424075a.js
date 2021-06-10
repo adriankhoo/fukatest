657c3dd5b15ea38eb54ad928bcac71b6f181b22e
adriank <adriankhoo@hotmail.com>
2021-06-10 22:34:25 +0800

    first commit

---
+++ precache-manifest.9fbe22042ba94f3a2aca55aaa424075a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf947593b834b494a643c1147f23904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1650fe13b389e8923b9",
+    "url": "/static/css/2.849a7a81.chunk.css"
+  },
+  {
+    "revision": "94a9443b939ee16c2be9",
+    "url": "/static/css/main.0e8aca70.chunk.css"
+  },
+  {
+    "revision": "e1650fe13b389e8923b9",
+    "url": "/static/js/2.a2705e1e.chunk.js"
+  },
+  {
+    "revision": "4389890eb2317c4bc3909cd27dc9a4b3",
+    "url": "/static/js/2.a2705e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a9443b939ee16c2be9",
+    "url": "/static/js/main.c21188f8.chunk.js"
+  },
+  {
+    "revision": "cd3bc823b1408d6fefd5",
+    "url": "/static/js/runtime-main.ca0b5860.js"
+  },
+  {
+    "revision": "ed58ab7fb3247a37d13f75944770433f",
+    "url": "/static/media/thermometer.ed58ab7f.svg"
+  },
+  {
+    "revision": "75a3c085b10ac083bde1ce1ce6944a53",
+    "url": "/static/media/valve.75a3c085.svg"
+  }
+]);
